d850bf602271ee8891d9a1a0c2c0cd96ba1da06a
RASEL KIBRIA <rasel5jkibria@gmail.com>
2022-03-23 01:59:03 +0600

    Deparment delete request working

--- StudentDeptApiAssignment/StudentDeptApiAssignment/Controllers/DepartmentController.cs
+++ StudentDeptApiAssignment/StudentDeptApiAssignment/Controllers/DepartmentController.cs
@@ -40,6 +40,17 @@ namespace StudentDeptApiAssignment.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, deptModel);
         }
 
-
+        [Route("api/dept/delete/{id}")]
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            ApiAssignmentEntities db = new ApiAssignmentEntities();
+            var department = (from dept in db.Departments
+                           where dept.Id.Equals(id)
+                           select dept).FirstOrDefault();
+            db.Departments.Remove(department);
+            db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+        }
     }
 }
